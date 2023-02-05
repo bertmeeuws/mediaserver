6ddbd8f013be50f9f6c80c28938f8047688e91f2
Bert Meeuws <bert.meeuws@inthepocket.com>
2023-02-05 11:39:11 +0100

    feat: can now query on path

--- src/main/scala/controllers/FileManagerController.scala
+++ src/main/scala/controllers/FileManagerController.scala
@@ -16,31 +16,62 @@ import com.domain.Router
 import com.domain.CORSHandler
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 
+import java.net.URLEncoder
+
 
 
 
 
 class FileManagerController(store: ActorRef[Nothing])(val system: ActorSystem[_]) extends FailFastCirceSupport with Router {
-  var currentPath = "/Users/bertmeeuws/Movies"
+  private val mainPath = "/Users/bertmeeuws/Movies"
 
 
   import com.models.FileManagerModels._
   import com.auth.Utils._
 
+  def loadLibrary(path: Option[String]): Either[String,List[FileItem]] = {
+
+    val queriedPath = path.getOrElse(mainPath)
+
+    val dir = new File(queriedPath)
+
+    val doesPathExist = dir.exists()
+
+    if(doesPathExist){
+      val rawFiles = dir.listFiles()
+
+      val libraryFiles = rawFiles.toList.foldLeft[List[FileItem]](List())((acc: List[FileItem], file: File) => {
+        val fileItem = FileItem(fileName = file.getName, filePath = file.getPath, isFolder = file.isDirectory)
+
+        fileItem :: acc
+      })
+
+      Right(libraryFiles)
+    } else {
+      Left("The file path does not exist")
+    }
+  }
+
   override val routes: Route = cors() {
-    path("filemanager") {
+    pathPrefix("filemanager") {
       authenticateBasic(realm = "secure site", authenticator) { userName =>
       pathEnd {
-        val dir = new File(currentPath)
+        val libraryFiles = loadLibrary(None)
 
-        val libraryFiles = dir.listFiles().toList.foldLeft[List[FileItem]](List())((acc: List[FileItem], file: File)  => {
-          val fileItem = FileItem(fileName = file.getName, filePath = file.getPath, isFolder = file.isDirectory)
-
-          fileItem :: acc
-        })
-
-          complete(StatusCodes.OK, libraryFiles)
+        libraryFiles match {
+          case Left(a) => complete(StatusCodes.BadRequest, a)
+          case Right(b) => complete(StatusCodes.OK, b)
         }
+
+        } ~ path(Remaining) { path => {
+        val libraryFiles = loadLibrary(Some(path))
+
+
+        libraryFiles match {
+          case Left(a) => complete(StatusCodes.BadRequest, a)
+          case Right(b) => complete(StatusCodes.OK, b)
+        }
+      }}
       }
     }
   }
